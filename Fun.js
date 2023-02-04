19c334f62e21d46891ba1e4a3d9a60d3baf22e53
obiect10 <124463019+obiect10@users.noreply.github.com>
2023-02-04 20:09:05 +0100

    Add files via upload

---
+++ Fun.js
@@ -0,0 +1,20 @@
+let x = Math.floor(Math.random() * 6) + 1;
+if (x === 1){
+    document.write(`⚀`);
+}
+if (x === 2){
+    document.write(`⚁`);
+}
+if (x === 3){
+    document.write(`⚂`);
+}
+if (x === 4){
+    document.write(`⚃`);
+}
+if (x === 5){
+    document.write(`⚄`);
+}
+if (x === 6){
+    document.write(`⚅
+    ?.zostań doceniony przez komitet cieni i wciągnięty do świata elit, zaczynając nową karierę rządzenia światem`);
+}
